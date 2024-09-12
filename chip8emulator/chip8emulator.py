5d73c9078a58610d0c07133fc23d913d704cf907
manolo <mmartinortiz@gmail.com>
2024-09-12 09:52:57 +0200

    Arcade library

--- chip8emulator/chip8emulator.py
+++ chip8emulator/chip8emulator.py
@@ -1,20 +1,46 @@
+import arcade
+
 from chip8emulator.memory import Memory
 from chip8emulator.processor import Processor
 
 
-def chip8loop():
-    memory = Memory(size=4096)
-    processor = Processor(memory=memory)
-    processor.reset()
+class Chip8Emulator(arcade.Window):
+    def __init__(self):
+        super().__init__(640, 320, "Chip-8 Emulator")
+        self.memory = Memory(size=4096)
+        self.processor = Processor(memory=self.memory)
+        self.processor.reset()
 
-    while True:
+    def setup(self):
+        # Set up the game
+        pass
+
+    def on_draw(self):
+        # Draw the screen
+        pass
+
+    def on_key_press(self, key, modifiers):
+        # Press the key
+        pass
+
+    def on_key_release(self, key, modifiers):
+        # Release the key
+        pass
+
+    def update(self, delta_time):
         # Emulate one cycle
-        processor.cycle()
+        self.processor.cycle()
 
         # If the draw flag is set, update the screen
 
         # Store key press state (Press and Release)
 
 
+def main():
+    window = Chip8Emulator()
+    window.setup()
+    arcade.run()
+
+
 if __name__ == "__main__":
-    chip8loop()
+    main()
