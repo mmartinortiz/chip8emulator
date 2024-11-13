330a505b9db98404e71ad5f0f5351901e454fddc
manolo <mmartinortiz@gmail.com>
2024-11-13 19:00:29 +0000

    Use bitarray to handle the pixels

--- chip8emulator/graphics.py
+++ chip8emulator/graphics.py
@@ -1,15 +1,26 @@
+from bitarray import bitarray
+from bitarray.util import ba2int
+
+
 class Graphics:
     def __init__(self, width: int = 64, height: int = 32) -> None:
+        """
+        Graphic interface for the Chip-8 emulator. Width and Height are provided for
+        testing purposes, since the original Chip-8 had a screen size of 64x32 pixels.
+
+        Args:
+            width (int, optional): Screen width, in bits. Defaults to 64.
+            height (int, optional): Screen height, in bits. Defaults to 32.
+        """
         self.width = width
         self.height = height
-
-        self.pixels = [[0] * width for _ in range(height)]
+        self.clear()
 
     def clear(self) -> None:
         """
         Clear the screen by setting all pixels to 0.
         """
-        self.pixels = [[0] * self.width for _ in range(self.height)]
+        self.pixels = bitarray(self.width * self.height)
 
     def get(self, x: int, y: int) -> int:
         """
@@ -23,7 +34,7 @@ class Graphics:
         Returns:
             int: Value of the pixel at the specified coordinates.
         """
-        return self.pixels[y][x]
+        return self.pixels[x + (self.width * y)]
 
     def set(self, x: int, y: int, value: int) -> None:
         """
@@ -33,11 +44,22 @@ class Graphics:
         Args:
             x (int): X coordinate.
             y (int): Y coordinate.
-            value (int): Value to set the pixel to.
+            value (int): Value to set the pixel-byte to.
         """
-        self.pixels[y][x] = value
+        self.pixels[x + (self.width * y)] = value
+
+    def as_list_of_integers(self) -> list[int]:
+        """
+        Convert the pixel values to a list of integers.
+        """
+        return [ba2int(self.pixels[i : i + 8]) for i in range(0, len(self.pixels), 8)]
 
     def __repr__(self) -> str:
-        return "\n".join(
-            "".join("X" if pixel == 1 else "." for pixel in row) for row in self.pixels
-        )
+        """
+        Print the graphic's pixels as a string of 1s and 0s.
+        """
+        rows = []
+        for row in range(0, self.height * self.width, self.width):
+            rows.append(self.pixels[row : row + self.width].to01())
+
+        return "\n".join(rows)
--- tests/test_graphics.py
+++ tests/test_graphics.py
@@ -1,28 +1,27 @@
 import pytest
+from bitarray import bitarray
 
 from chip8emulator.graphics import Graphics
 
 
 def test_graphics_clear():
     graphics = Graphics()
-    graphics.pixels = [[1] * 64 for _ in range(32)]
-
     graphics.clear()
 
-    assert all(pixel == 0 for row in graphics.pixels for pixel in row)
+    assert all(pixel == 0x00 for pixel in graphics.pixels)
 
 
 @pytest.mark.parametrize(
     "pixels, x, y, expected",
     [
-        ([list("abcd"), list("efgh"), list("ijkl"), list("mnop")], 0, 0, "a"),
-        ([list("abcd"), list("efgh"), list("ijkl"), list("mnop")], 1, 0, "b"),
-        ([list("abcd"), list("efgh"), list("ijkl"), list("mnop")], 0, 1, "e"),
-        ([list("abcd"), list("efgh"), list("ijkl"), list("mnop")], 1, 1, "f"),
+        (bitarray("10100101"), 0, 0, 1),
+        (bitarray("10100101"), 3, 0, 0),
+        (bitarray("10100101"), 0, 1, 0),
+        (bitarray("10100101"), 3, 1, 1),
     ],
 )
 def test_graphics_get(pixels, x, y, expected):
-    graphics = Graphics()
+    graphics = Graphics(width=4, height=2)
     graphics.pixels = pixels
 
     assert graphics.get(x, y) == expected
@@ -31,39 +30,41 @@ def test_graphics_get(pixels, x, y, expected):
 @pytest.mark.parametrize(
     "pixels, x, y, value, expected",
     [
-        (
-            [list("abcd"), list("efgh"), list("ijkl"), list("mnop")],
-            0,
-            0,
-            1,
-            [[1, "b"], ["e", "f"], ["i", "j"], ["m", "n"]],
-        ),
-        (
-            [list("abcd"), list("efgh"), list("ijkl"), list("mnop")],
-            1,
-            0,
-            1,
-            [["a", 1], ["e", "f"], ["i", "j"], ["m", "n"]],
-        ),
-        (
-            [list("abcd"), list("efgh"), list("ijkl"), list("mnop")],
-            0,
-            1,
-            1,
-            [["a", "b"], [1, "f"], ["i", "j"], ["m", "n"]],
-        ),
-        (
-            [list("abcd"), list("efgh"), list("ijkl"), list("mnop")],
-            1,
-            1,
-            1,
-            [["a", "b"], ["e", 1], ["i", "j"], ["m", "n"]],
-        ),
+        (bitarray(4 * 2), 0, 0, 1, "10000000"),
+        (bitarray(4 * 2), 3, 0, 1, "00010000"),
+        (bitarray(4 * 2), 0, 1, 1, "00001000"),
+        (bitarray(4 * 2), 3, 1, 1, "00000001"),
     ],
 )
 def test_graphics_set(pixels, x, y, value, expected):
-    graphics = Graphics()
+    graphics = Graphics(4, 2)
     graphics.pixels = pixels
 
     graphics.set(x, y, value)
     graphics.pixels == expected
+
+
+@pytest.mark.parametrize(
+    "pixels, expected",
+    [
+        (bitarray("1010101001010101"), [0xAA, 0x55]),
+    ],
+)
+def test_as_list_of_integers(pixels, expected):
+    graphics = Graphics(8, 2)
+    graphics.pixels = pixels
+
+    assert graphics.as_list_of_integers() == expected
+
+
+@pytest.mark.parametrize(
+    "pixels, expected",
+    [
+        (bitarray("10100101"), "1010\n0101"),
+    ],
+)
+def test_print(pixels, expected):
+    graphics = Graphics(4, 2)
+    graphics.pixels = pixels
+
+    assert str(graphics) == expected
