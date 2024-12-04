9a5805c3402299c90db41a3fde0f1f306c92e472
manolo <mmartinortiz@gmail.com>
2024-12-04 21:45:09 +0000

    Fix uv completions for the fish shell

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uv generate-shell-completion fish | source
+uv generate-shell-completion fish > ~/.config/fish/completions/uv.fish
 uv sync --link-mode=copy
 /workspaces/chip8emulator/.venv/bin/pre-commit install --install-hooks
 /workspaces/chip8emulator/.venv/bin/pre-commit autoupdate
