90f451f5e593f17a5a97f4d8471f31d9aac99f02
manolo <mmartinortiz@gmail.com>
2024-12-04 21:35:41 +0000

    uv completions

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+uv generate-shell-completion fish | source
 uv sync --link-mode=copy
 /workspaces/chip8emulator/.venv/bin/pre-commit install --install-hooks
 /workspaces/chip8emulator/.venv/bin/pre-commit autoupdate
