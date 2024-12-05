1bce07ec6e569d71392fb0f7ea0eab52ff33acaf
manolo <mmartinortiz@gmail.com>
2024-12-05 14:18:33 +0000

    Eye candy for the command line

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "
+format = \"\$git_branch \$git_status\\n> \"
+" > ~/.config/starship.toml
+echo "
+starship init fish | source
+" >> ~/.config/fish/config.fish
 uv generate-shell-completion fish > ~/.config/fish/completions/uv.fish
 uv sync --link-mode=copy
 /workspaces/chip8emulator/.venv/bin/pre-commit install --install-hooks
