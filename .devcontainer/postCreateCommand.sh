6ab2a7e07c37bd69fbdc5dea2cb3b0fb4d56d1fa
manolo <mmartinortiz@gmail.com>
2025-01-29 09:17:18 +0000

    build: run tox and pre-commit as external tools

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,7 +5,3 @@ format = \"\$git_branch \$git_status\\n> \"
 echo "
 starship init fish | source
 " >> ~/.config/fish/config.fish
-uv generate-shell-completion fish > ~/.config/fish/completions/uv.fish
-uv sync --link-mode=copy
-/workspaces/chip8emulator/.venv/bin/pre-commit install --install-hooks
-/workspaces/chip8emulator/.venv/bin/pre-commit autoupdate
